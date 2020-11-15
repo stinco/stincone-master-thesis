b55744ad382cc5638c05b21ae9f8cda7804a2d44
Leonardo Stincone <stinconeleonardo@gmail.com>
2020-11-16 00:40:34 +0100

    some writing

---
+++ trials/02_prova_distributions.R
@@ -0,0 +1,120 @@
+library(tidyverse)
+library(knitr)
+library(kableExtra)
+
+theme_set(theme_bw())
+
+
+
+
+# Poisson ####
+
+lambda = 2.5
+
+tibble(
+  x = 0:20
+) %>% 
+  mutate(
+    y = dpois(x = x, lambda = lambda)
+  ) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  geom_segment(aes(xend = x),
+               yend = 0) +
+  labs(x = "k", y = "p(k)",
+       title = "Poisson Distribution",
+       subtitle = str_c("lambda = ", lambda))
+
+
+
+# tibble(x = 0:20) %>% 
+#   crossing(
+#     tibble(
+#       lambda = c(.8, 1, 2.5, 10)
+#     )
+#   ) %>% 
+#   mutate(
+#     y = dpois(x = x, lambda = lambda),
+#     lambda_label = str_c("lambda = ", lambda) %>% 
+#       fct_inorder()
+#   ) %>% 
+#   ggplot(aes(x = x, y = y)) +
+#   geom_point() +
+#   geom_segment(
+#     aes(xend = x),
+#     yend = 0
+#   ) +
+#   facet_wrap(~lambda_label) +
+#   labs(
+#     x = "k", y = "p(k)",
+#     title = "Poisson Distribution"
+#   )
+
+tibble(x = 0:22) %>% 
+  crossing(
+    tibble(
+      lambda = c(.8, 1, 2.5, 10)
+    )
+  ) %>% 
+  mutate(
+    y = dpois(x = x, lambda = lambda),
+    lambda_label = str_c("lambda = ", lambda) %>% 
+      fct_inorder()
+  ) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_point() +
+  geom_segment(
+    aes(xend = x),
+    yend = 0
+  ) +
+  facet_wrap(~lambda_label) +
+  coord_cartesian(
+    xlim = c(0, 20)
+  ) +
+  labs(
+    x = "n", y = "p(n)"
+  )
+
+
+# Gamma ####
+
+
+tibble(x = seq(from = 0, to = 25, by = .01)) %>% 
+  crossing(
+    tibble(
+      alpha = c(.8, 1, 2, 16),
+      rho = c(.2, .25, .5, 2)
+    )
+  ) %>% 
+  mutate(
+    y = dgamma(x = x, shape = alpha, rate = rho),
+    label = str_c("alpha = ", alpha, ", rho = ", rho) %>% 
+      fct_inorder()
+  ) %>% 
+  ggplot(aes(x = x, y = y)) +
+  geom_line() +
+  facet_wrap(~label) +
+  coord_cartesian(
+    ylim = c(0, .3),
+    xlim = c(0, 20)
+  ) +
+  labs(
+    x = "z", y = "f(z)"
+  )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
